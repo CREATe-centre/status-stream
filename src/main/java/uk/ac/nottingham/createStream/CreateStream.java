5dd0547ab0544983dbc5958c7bb85f74d67ce77d
Javid Yousaf <javidyousaf@users.noreply.github.com>
2015-11-09 11:04:28 +0000

    Removed access token and secret.

--- src/main/java/uk/ac/nottingham/createStream/CreateStream.java
+++ src/main/java/uk/ac/nottingham/createStream/CreateStream.java
@@ -10,8 +10,8 @@ public class CreateStream {
 
 //		final String accessToken = args[0];
 //		final String accessTokenSecret = args[1];		
-		final String accessToken = "3216709625-Ts39k0LQsySucbf2Z4BuioZp6iQj1dYkQ8YYTOb";
-		final String accessTokenSecret = "3Oz7RJWKkdrs1coqNjeXtmXuFyWC6jV0uLeCsNM8GOUSO";		
+		final String accessToken = "";
+		final String accessTokenSecret = "";		
 		GetStream stream = new GetStream();
 		stream.createStream(accessToken, accessTokenSecret);
 	}
