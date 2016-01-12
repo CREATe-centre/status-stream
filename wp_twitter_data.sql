28d93364739f25aa994c9820de96bbf1d46d4848
Dominic Price <dominic.price@nottingham.ac.uk>
2016-01-12 14:05:05 +0000

    Removed old file no longer needed

--- wp_twitter_data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `wp_twitter_data` (
-  `id` bigint(255) NOT NULL AUTO_INCREMENT,
-  `userid` bigint(50) NOT NULL,
-  `event` varchar(25) NOT NULL,
-  `JSONdata` varchar(10000) NOT NULL,
-  `created_datetime` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-);
